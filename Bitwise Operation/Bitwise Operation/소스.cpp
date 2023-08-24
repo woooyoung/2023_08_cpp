a7cd4d2f0154b4e357b1398635416eafa0730edf
woooyoung <axdsw121@gmail.com>
2023-08-24 10:25:55 +0900

    비트와이즈 연산 02(카테고리 분류)

--- Bitwise Operation/Bitwise Operation/소스.cpp
+++ Bitwise Operation/Bitwise Operation/소스.cpp
@@ -1,20 +1,72 @@
-// 비트와이즈 연산 -> 비트를 기준으로 연산
+#include <cstdio>
+// 비트와이즈 연산 -> 비트를 기준으로 연산 -> 기계의 근본에 해당하는 연산
 // Bitwise or -> |
 // Bitwise and -> &
-// Shift
+// Shift <<  >>
 
-#include <cstdio>
+// 연산 속도(빠른순으로) : 더하기 빼기 곱하기 나누기 
+//                     : 정수연산 실수연산
+
+// 카테고리 분류
+
+void exam1(int params) {
+	int category1 = 1; // 1
+	int category2 = 1 << 1; //2
+	int category3 = 1 << 2; // 4
+
+	if ((category1 & params) != 0) {
+		printf("카테고리 1 에 포함됨\n");
+	}
+
+	if ((category2 & params) != 0) {
+		printf("카테고리 2 에 포함됨\n");
+	}
+
+	if ((category3 & params) != 0) {
+		printf("카테고리 3 에 포함됨\n");
+	}
+}
+
 
 int main() {
 
-	int v1 = 1;
-	int v2 = 3;
+	int mario_category = 8 + 4 + 2 + 1;
+
+	int enemy_category = 0 + 4 + 2 + 1;
+
+	int turtle_category = 0 + 0 + 2 + 1;
+
+	int structure_category = 0 + 4 + 0 + 0;
+
+	exam1(1 | 2 | 4);
+	exam1(structure_category);
+
+
+	int v1 = 1; // 0001
+	int v2 = 3; // 0011
 
 	int bitwise_or = v1 | v2;
-	printf("%d\n", bitwise_or);
+	printf("v1 | v2 : %d\n", bitwise_or);
 
 	int bitwise_and = v1 & v2;
-	printf("%d\n", bitwise_and);
+	printf("v1 & v2 : %d\n", bitwise_and);
+
+	int shift_left = v2 << 1; // 0110
+	printf("v2 << 1 : %d\n", shift_left);
+
+	int shift_right = v2 >> 1; // 0001
+	printf("v2 >> 1 : %d\n", shift_right);
+
+	//int v3 = 1; // 0001
+	//int v4 = 2; // 0010
+
+	//int bitwise_or2 = v3 | v4;
+	//printf("%d\n", bitwise_or2);
+
+	//int bitwise_and2 = v3 & v4;
+	//printf("%d\n", bitwise_and2);
+
+
 
 	return 0;
 }
---
+++ Bitwise Operation/Bitwise Operation/소스1.cpp
@@ -0,0 +1,17 @@
+// 카테고리 분류 -> 게임의 물리엔진이나 물리분야의 시뮬레이션(가장 빠르기 때문에)
+// 충돌체크, 반발력 체크, 마찰력 체크
+#include <cstdio>
+
+
+int main() {
+
+	int mario_category = 8 + 4 + 2 + 1;
+
+	int enemy_category = 0 + 4 + 2 + 1;
+
+	int turtle_category = 0 + 0 + 2 + 1;
+
+	int structure_category = 0 + 4 + 0 + 0;
+
+	return 0;
+}
